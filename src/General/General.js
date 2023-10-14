55b3323af54d0475c33c91a5976ce4c234af64f9
Lasarussp <001lasarus@gmail.com>
2023-10-14 12:28:40 +0530

    Create files,folders,api added styles

---
+++ src/General/General.js
@@ -0,0 +1,11 @@
+export const API = 'http://localhost:5000';
+//export const API = "https://hackathon2-backend.vercel.app";
+
+export const LinkStyling = {
+  textDecoration: "none",
+  color: "inherit",
+};
+
+export const githubLink = "";
+export const twitterLink = "";
+export const linkedinLink = "";
